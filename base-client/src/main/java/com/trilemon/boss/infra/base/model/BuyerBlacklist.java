765cdd8d5fc3394eef8e00ad090115f222163e74
kevin <guyan.data@gmail.com>
2013-11-20 18:03:45 +0800

    添加黑名单表

---
+++ base-client/src/main/java/com/trilemon/boss/infra/base/model/BuyerBlacklist.java
@@ -0,0 +1,85 @@
+package com.trilemon.boss.infra.base.model;
+
+import java.util.Date;
+
+public class BuyerBlacklist {
+    private Long id;
+
+    private Long userId;
+
+    private String buyerNick;
+
+    private Byte type;
+
+    private Byte status;
+
+    private Integer filterCount;
+
+    private Date addTime;
+
+    private Date updTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getBuyerNick() {
+        return buyerNick;
+    }
+
+    public void setBuyerNick(String buyerNick) {
+        this.buyerNick = buyerNick == null ? null : buyerNick.trim();
+    }
+
+    public Byte getType() {
+        return type;
+    }
+
+    public void setType(Byte type) {
+        this.type = type;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Integer getFilterCount() {
+        return filterCount;
+    }
+
+    public void setFilterCount(Integer filterCount) {
+        this.filterCount = filterCount;
+    }
+
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+
+    public Date getUpdTime() {
+        return updTime;
+    }
+
+    public void setUpdTime(Date updTime) {
+        this.updTime = updTime;
+    }
+}
---
+++ base/src/main/java/com/trilemon/boss/infra/base/dao/BuyerBlacklistMapper.java
@@ -0,0 +1,31 @@
+package com.trilemon.boss.infra.base.dao;
+
+import com.trilemon.boss.infra.base.model.BuyerBlacklist;
+import com.trilemon.boss.infra.base.model.BuyerBlacklistExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BuyerBlacklistMapper {
+    int countByExample(BuyerBlacklistExample example);
+
+    int deleteByExample(BuyerBlacklistExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(BuyerBlacklist record);
+
+    int insertSelective(BuyerBlacklist record);
+
+    List<BuyerBlacklist> selectByExample(BuyerBlacklistExample example);
+
+    BuyerBlacklist selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") BuyerBlacklist record, @Param("example") BuyerBlacklistExample example);
+
+    int updateByExample(@Param("record") BuyerBlacklist record, @Param("example") BuyerBlacklistExample example);
+
+    int updateByPrimaryKeySelective(BuyerBlacklist record);
+
+    int updateByPrimaryKey(BuyerBlacklist record);
+}
